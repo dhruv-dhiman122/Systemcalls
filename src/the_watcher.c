1bbb5d05a43217ddc4d6c9b55efb6c9fcfc36eb0
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-10-19 21:50:46 +0530

    The code space is defined and orgainzed, and the exit success and exit failure is defined

--- src/the_watcher.c
+++ src/the_watcher.c
@@ -4,11 +4,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+//--------------- After this lines no more includes or any thing that is to do with compilers --------------------------//
+
 //------------------- Area for defines or macro or for operations before compiler kicks in -----------------------------------//
 
-#define EXT_SUCCESS 0
+#define exit_success 0
+#define exit_failure 1
 
-//--------------- After this lines no more includes or any thing that is to do with compilers --------------------------
 // Area for global variables or namespaces (if any)
 
 //------------------ After this line the code is there written ----------------------------------------
@@ -17,5 +19,5 @@
 // ---------------------- space for main function only --------------------------------
 int main() {
 
-		exit(EXT_SUCCESS);
+		exit(exit_success);
 }
