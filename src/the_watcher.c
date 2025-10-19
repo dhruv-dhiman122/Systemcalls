c179ea35bc37a274d74a1df1d8cdd08053c17c42
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-10-19 21:25:11 +0530

    The srouce file is made

---
+++ src/the_watcher.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+//------------------- Area for defines or macro or for operations before compiler kicks in -----------------------------------//
+
+#define EXT_SUCCESS 0
+
+//--------------- After this lines no more includes or any thing that is to do with compilers --------------------------
+// Area for global variables or namespaces (if any)
+
+//------------------ After this line the code is there written ----------------------------------------
+// space for any function that are needed to be made by the user
+
+// ---------------------- space for main function only --------------------------------
+int main() {
+
+		exit(EXT_SUCCESS);
+}
