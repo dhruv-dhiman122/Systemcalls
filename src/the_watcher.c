f83447c75508d30364bf1b675bafe1fda9c54cb1
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-10-21 09:51:37 +0530

    the updation is done to code format in src folder

--- src/the_watcher.c
+++ src/the_watcher.c
@@ -4,17 +4,16 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-//--------------- After this lines no more includes or any thing that is to do with compilers --------------------------//
+//--------------- After this line there must not be any more inclusion --------------------------//
 
-//------------------- Area for defines or macro or for operations before compiler kicks in -----------------------------------//
+//------------------- Area for macro or for operations before compiler kicks in -----------------------------------//
 
 #define exit_success 0
 #define exit_failure 1
 
-// Area for global variables or namespaces (if any)
+//---------------------------- Area for global variables or namespaces (if any) ----------------------------------//
 
-//------------------ After this line the code is there written ----------------------------------------
-// space for any function that are needed to be made by the user
+//------------------ After this line the code is there written any function that is made by the user ----------------------------------------//
 
 // ---------------------- space for main function only --------------------------------
 int main() {
