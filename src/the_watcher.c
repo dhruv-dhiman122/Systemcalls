8d224598698648eb11ecc3d2727e29d86d66e749
dhruv dhiman <dhruvdhiman770@gmail.com>
2025-10-24 19:02:08 +0530

    learning: I have to understand memory management. Code: The code was improved using AI and its gcc file is also made

--- src/the_watcher.c
+++ src/the_watcher.c
@@ -11,21 +11,51 @@
 
 #define exit_success 0
 #define exit_failure 1
-
+#define TOO_FEW_ARGUMENTS 1
 //---------------------------- Area for global variables or namespaces (if any) ----------------------------------//
 
 //------------------ After this line the code is there written any function that is made by the user ----------------------------------------//
 
 // ---------------------- space for main function only --------------------------------
-int main(int argc,char** argv) {
+int main(int argc, char** argv) {
 		char *basePath = NULL;
-
-		if(argc < 2) {
-				fprintf(stderr,"There is a error, the argument are less than 2. You atleast want one argument");
-				exit(exit_failure);
+		char *token = NULL;
+    
+		if (argc < 2) {
+				fprintf(stderr, "USAGE: the_watcher path\n");
+				exit(TOO_FEW_ARGUMENTS);
 		}
 
-		basePath = (char*)malloc(sizeof(char)*strlen(argv[1])+1);
+		// Copy the input string properly
+		basePath = (char*)malloc(strlen(argv[1]) + 1);
+    
+		if (basePath == NULL) {
+				perror("malloc failed");
+				exit(exit_failure);
+		}
+		strcpy(basePath, argv[1]);
 
+		// Tokenize to get the last component
+		token = strtok(basePath, "/");
+		char *lastToken = token;
+		
+		while (token != NULL) {
+				lastToken = token;  // Keep track of the last token
+				token = strtok(NULL, "/");
+		}
+    
+		if (lastToken != NULL) {
+				printf("%s\n", lastToken);
+		} 
+		else {
+				printf("\n");
+		}
+		
+		while(true) // because the system calls run behind the sense continue 
+		{
+		
+		}
+
+		free(basePath);
 		exit(exit_success);
 }
